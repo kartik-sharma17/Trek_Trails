f5e6f29f3103e060b0becf1757fcb2831bd5d86e
kartik sharma <kartiksharma88103@gmail.com>
2024-10-10 17:32:34 +0530

    initialize backend files

---
+++ Back_end/src/main/java/Back_end/trek_trails/TrekTrailsApplication.java
@@ -0,0 +1,13 @@
+package Back_end.trek_trails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrekTrailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrekTrailsApplication.class, args);
+	}
+
+}
---
+++ Back_end/src/test/java/Back_end/trek_trails/TrekTrailsApplicationTests.java
@@ -0,0 +1,13 @@
+package Back_end.trek_trails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrekTrailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
